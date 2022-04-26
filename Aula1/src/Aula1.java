4943e23545870323486c515f44b11582f4503691
Hadra Sejas <hsejas@furb.br>
2022-04-26 18:08:35 -0300

    aula1

---
+++ Aula1/src/Aula1.java
@@ -0,0 +1,22 @@
+public class Aula1 {
+
+    //comentário de linha
+    /*cometário de bloco
+    block
+    block
+    block
+     */
+    static int var1 = 10;
+    double var2 = 25.8;
+    char var3 = 'a';
+    String var4 = "ajksdhk";
+    boolean var5 = true;
+
+    public static void main(String[] args) {
+        int var1 = 10;
+        String var2 = "10";
+        System.out.println(var1 + var2);
+    }
+    //private
+    //protected
+}
